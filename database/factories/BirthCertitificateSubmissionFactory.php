83cd3acb1ed3c2a7245bd465f5de1f08319257f3
All Binardo <all.binardo@ifabula.com>
2020-09-21 13:28:36 +0700

    deploy

--- database/factories/BirthCertitificateSubmissionFactory.php
+++ database/factories/BirthCertitificateSubmissionFactory.php
@@ -2,11 +2,20 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Model;
+use App\BirthCertificateSubmission;
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(BirthCertificateSubmission::class, function (Faker $faker) {
     return [
-        //
+        "id_card" => $faker->randomNumber(9),
+        "name" => $faker->name,
+        "gender" => $faker->randomElement(['laki-laki', 'perempuan']),
+        "birthplace" => $faker->city,
+        "family_card_number" => $faker->randomNumber(9),
+        "birthdate" => '1990-01-01',
+        "mother_identity_card_number" => $faker->randomNumber(9),
+        "mother_name" => $faker->name(),
+        "father_identity_card_number" => $faker->randomNumber(9),
+        "father_name" => $faker->name(),
     ];
 });
