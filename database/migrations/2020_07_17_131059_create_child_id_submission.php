1f2904fbd999d655dad8b4baa4e7791cd2d8bcad
All Binardo <all.binardo@ifabula.com>
2020-09-21 14:17:56 +0700

    refactor

--- database/migrations/2020_07_17_131059_create_child_id_submission.php
+++ database/migrations/2020_07_17_131059_create_child_id_submission.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use App\Enums\DocumentStatus;
+use App\enums\DocumentStatus;
 
 class CreateChildIdSubmission extends Migration
 {
