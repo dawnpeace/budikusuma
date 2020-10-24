6b821ad42de31579663da23f4c22809d6f0cc931
All Binardo <all.binardo@ifabula.com>
2020-10-24 19:44:30 +0700

    phase 1

--- app/Exports/ChildIdentityCardExport.php
+++ app/Exports/ChildIdentityCardExport.php
@@ -4,10 +4,10 @@ namespace App\Exports;
 
 use App\ChildIdentityCard;
 use Maatwebsite\Excel\Concerns\Exportable;
-use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\FromQuery;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithMapping;
+use Carbon\Carbon;
 
 class ChildIdentityCardExport implements FromQuery, WithMapping, WithHeadings
 {
@@ -30,11 +30,45 @@ class ChildIdentityCardExport implements FromQuery, WithMapping, WithHeadings
 
     public function map($row) : array
     {
-        return [];
+        return [
+            $row->card_number,
+            $row->name,
+            $row->gender,
+            $row->family_card_number,
+            $row->birth_certificate_number,
+            $row->birthplace,
+            Carbon::createFromFormat('Y-m-d  H:i:s', $row->birthdate)->format('d-m-Y'),
+            $row->address,
+            $row->householder_name,
+            $row->religion,
+            $row->rt,
+            $row->rw,
+            $row->kelurahan,
+            $row->kecamatan,
+            $row->citizenship,
+            Carbon::createFromFormat('Y-m-d H:i:s', $row->created_at)->format('d-m-Y'),
+        ];
     }
 
     public function headings() : array
     {
-        return [];
+        return [
+            "No KIA",
+            "Nama",
+            "Jenis Kelamin",
+            "No KK",
+            "No Akta Lahir",
+            "Tempat Lahir",
+            "Tanggal Lahir",
+            "Alamat",
+            "Nama Kepala Keluarga",
+            "Agama",
+            "RT",
+            "RW",
+            "Kelurahan",
+            "Kecamatan",
+            "Kewarganegaraan",
+            "Tanggal Arsip"
+        ];
     }
 }
